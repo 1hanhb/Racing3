e50a02253aa7a4d85abc44446eb888c38c4a2f73
Munak <oiool@naver.com>
2017-04-09 00:01:41 +0900

    2017.04.07 프로필 사진 완료 + 로딩화면 추가 2017.04.08 이미지 겹치기 추가 2017.04.09 트위터 라인 인스타 공유 추가

--- app/src/main/java/com/example/munak/comptest/MainActivity.java
+++ app/src/main/java/com/example/munak/comptest/MainActivity.java
@@ -1,15 +1,26 @@
 package com.example.munak.comptest;
 
+import android.Manifest;
+import android.content.ActivityNotFoundException;
 import android.content.Intent;
 import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.drawable.ShapeDrawable;
 import android.graphics.drawable.shapes.OvalShape;
+import android.media.Image;
 import android.net.Uri;
+import android.os.Environment;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.facebook.appevents.AppEventsLogger;
 import com.facebook.share.model.ShareLinkContent;
@@ -20,7 +31,18 @@ import com.kakao.util.KakaoParameterException;
 
 import com.facebook.FacebookSdk;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+
 public class MainActivity extends AppCompatActivity {
+
+    //permissionRequestCode
+    private static final int REQUEST_EXTERNAL_STORAGE_CODE = 1;
+    boolean permissionCheck = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,6 +57,45 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        Button mainTmapButton = (Button) findViewById(R.id.mainTmapButton);
+        mainTmapButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String appPackageName = "com.skt.tmap.ku"; // getPackageName() from Context or Activity object
+                try {
+                    startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("market://details?id=" + appPackageName)));
+                } catch (android.content.ActivityNotFoundException anfe) {
+                    startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://play.google.com/store/apps/details?id=" + appPackageName)));
+                }
+
+            }
+        });
+
+        ImageButton mainTwitter = (ImageButton) findViewById(R.id.mainTwitter);
+        mainTwitter.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                shareTwitter();
+            }
+        });
+
+        ImageButton mainLine = (ImageButton) findViewById(R.id.mainLine);
+        mainLine.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                shareLine();
+            }
+        });
+
+        ImageButton mainInstagram = (ImageButton) findViewById(R.id.mainInstagram);
+        mainInstagram.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                shareInstagram();
+            }
+        });
+
 
         FacebookSdk.sdkInitialize(getApplicationContext());
         AppEventsLogger.activateApp(this);
@@ -84,4 +145,125 @@ public class MainActivity extends AppCompatActivity {
         shareDialog.show(content, ShareDialog.Mode.FEED);   //AUTOMATIC, FEED, NATIVE, WEB 등이 있으며 이는 다이얼로그 형식을 말합니다.
     }
 
+    public void shareTwitter() {
+        String strLink = null;
+        try {
+            strLink = String.format("http://twitter.com/intent/tweet?text=%s", URLEncoder.encode("공유할 텍스트를 입력하세요", "utf-8"));
+        } catch (UnsupportedEncodingException e1) {
+            e1.printStackTrace();
+        }
+
+        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(strLink));
+        startActivity(intent);
+    }
+
+    public void shareLine()
+    {
+        try {
+            String text = "line://msg/text/" + "메시지를 입력하세요";
+            text = text.replaceAll("\n", "");
+            Intent intent = new Intent();
+            intent.setAction(Intent.ACTION_VIEW);
+            intent.setData(Uri.parse(text));
+            startActivity(intent);
+        }catch (ActivityNotFoundException e)
+        {
+            //앱 미설치 시
+            String appPackageName = "jp.naver.line.android"; // getPackageName() from Context or Activity object
+            try {
+                startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("market://details?id=" + appPackageName)));
+            } catch (android.content.ActivityNotFoundException anfe) {
+                startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://play.google.com/store/apps/details?id=" + appPackageName)));
+            }
+        }catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+    }
+
+    public void shareInstagram()
+    {
+        //외부저장 권한 요청(안드로이드 6.0 이후 필수)
+        onRequestPermission();
+
+        if(permissionCheck) {
+            Bitmap bm = BitmapFactory.decodeResource(getResources(), R.drawable.image);
+            String storage = Environment.getExternalStorageDirectory().getAbsolutePath();
+            String fileName = "이미지명.png";
+
+            String folderName = "/폴더명/";
+            String fullPath = storage + folderName;
+            File filePath;
+
+            try {
+                filePath = new File(fullPath);
+                if (!filePath.isDirectory()) {
+                    filePath.mkdirs();
+                }
+                FileOutputStream fos = new FileOutputStream(fullPath + fileName);
+                bm.compress(Bitmap.CompressFormat.PNG, 100, fos);
+                fos.flush();
+                fos.close();
+
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+
+            Intent share = new Intent(Intent.ACTION_SEND);
+            share.setType("image/*");
+            Uri uri = Uri.fromFile(new File(fullPath, fileName));
+            try {
+                share.putExtra(Intent.EXTRA_STREAM, uri);
+                share.putExtra(Intent.EXTRA_TEXT, "텍스트는 지원하지 않음!");
+                share.setPackage("com.instagram.android");
+                startActivity(share);
+            } catch (ActivityNotFoundException e) {
+                String appPackageName = "com.instagram.android"; // getPackageName() from Context or Activity object
+                try {
+                    startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("market://details?id=" + appPackageName)));
+                } catch (android.content.ActivityNotFoundException anfe) {
+                    startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://play.google.com/store/apps/details?id=" + appPackageName)));
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public void onRequestPermission()
+    {
+        int permissionReadStorage = ContextCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.READ_EXTERNAL_STORAGE);
+        int permissionWriteStorage = ContextCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.WRITE_EXTERNAL_STORAGE);
+
+        if(permissionReadStorage == PackageManager.PERMISSION_DENIED || permissionWriteStorage == PackageManager.PERMISSION_DENIED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.WRITE_EXTERNAL_STORAGE}, REQUEST_EXTERNAL_STORAGE_CODE);
+        }
+        else {
+            permissionCheck = true; //이미 허용되어 있으므로 PASS
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case REQUEST_EXTERNAL_STORAGE_CODE:
+                for (int i = 0; i < permissions.length; i++) {
+                    String permission = permissions[i];
+                    int grantResult = grantResults[i];
+                    if (permission.equals(Manifest.permission.READ_EXTERNAL_STORAGE)) {
+                        if(grantResult == PackageManager.PERMISSION_GRANTED) {
+                            //Toast.makeText(this, "허용했으니 가능함", Toast.LENGTH_LONG).show();
+                            permissionCheck = true;
+                        } else {
+                            //Toast.makeText(this, "허용하지 않으면 공유 못함 ㅋ", Toast.LENGTH_LONG).show();
+                            permissionCheck = false;
+                        }
+                    }
+                }
+                break;
+        }
+    }
 }
